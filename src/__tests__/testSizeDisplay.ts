1cc486044473228a689f194d84a21ea8d9d10907
Rick Wouters <rick.wouters@wolfpackit.nl>
2022-08-15 15:58:22 +0200

    Write some example tests

--- src/__tests__/testSizeDisplay.ts
+++ src/__tests__/testSizeDisplay.ts
@@ -1,12 +1,35 @@
 import '@testing-library/jest-dom';
 
-import { render, fireEvent, screen } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 
 import SizeDisplay from '../lib/SizeDisplay.svelte';
 
 test('shows bytes under a 1000 bytes', () => {
   render(SizeDisplay, { size: 50 });
-  const content = screen.getByText('50.00 B');
+  const size = screen.getByText('50.00 B');
+  expect(size).toBeInTheDocument();
+});
 
-  expect(content).toBeInTheDocument();
+test('shows kilobytes under 1000000 bytes', () => {
+  render(SizeDisplay, { size: 12344});
+  const size = screen.getByText('12.34 KB');
+  expect(size).toBeInTheDocument();
+});
+
+test('shows megabytes under 1000000000 bytes', () => {
+  render(SizeDisplay, { size: 12340000});
+  const size = screen.getByText('12.34 MB');
+  expect(size).toBeInTheDocument();
+});
+
+test('shows gigabytes under 1000000000000 bytes', () => {
+  render(SizeDisplay, { size: 12340000000 });
+  const size = screen.getByText('12.34 GB');
+  expect(size).toBeInTheDocument();
+});
+
+test('shows terabytes under 1000000000000000 bytes', () => {
+  render(SizeDisplay, { size: 12340000000000 });
+  const size = screen.getByText('12.34 TB');
+  expect(size).toBeInTheDocument();
 });
