8197c96e89cc193d763b0d6b690af5e32fe07f89
Ruben Brinkman <github@brnkmn.com>
2023-06-08 13:53:27 +0200

    test: not skipping playwright test

--- tests/appLoad.spec.ts
+++ tests/appLoad.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from '@playwright/test';
-test.skip('correctly loads the index page', async ({ page }) => {
+test('correctly loads the index page', async ({ page }) => {
 	await page.goto(`/`);
 	await expect(page.locator('css=.file-browser')).toBeVisible();
 });
